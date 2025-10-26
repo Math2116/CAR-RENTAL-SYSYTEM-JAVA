da2805d836e6b40cb3df090cf4205ada12eec8de
Mathew <mathewanujoy2116@gmail.com>
2025-10-26 18:42:51 +0530

    Add files via upload
    This is a simple console-based Car Rental System built in Java.
    It allows users to:
    
    View the list of available cars
    
    Rent a car
    
    Return a rented car
    
    Each car has a model name, rental price per day, and rental status.
    The system updates the availability of cars dynamically when rented or returned.
    
    This project demonstrates object-oriented programming concepts like:
    
    Classes and Objects (Car class)
    
    Encapsulation (car details and rental state)
    
    ArrayList for managing multiple cars
    
    Loops and Conditionals for program control
    
    Use case:
    Perfect for beginners to understand how OOP can be applied to real-world scenarios like managing car rental

---
+++ CarRentalSystem.java
@@ -0,0 +1,89 @@
+// File: CarRentalSystem.java
+import java.util.*;
+
+class Car {
+    String model;
+    double pricePerDay;
+    boolean isRented;
+
+    Car(String model, double pricePerDay) {
+        this.model = model;
+        this.pricePerDay = pricePerDay;
+        this.isRented = false;
+    }
+}
+
+public class CarRentalSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Honda City", 2000));
+        cars.add(new Car("Maruti Swift", 1500));
+        cars.add(new Car("Toyota Fortuner", 3500));
+
+        int choice;
+        do {
+            System.out.println("\n===== CAR RENTAL SYSTEM =====");
+            System.out.println("1. View Available Cars");
+            System.out.println("2. Rent a Car");
+            System.out.println("3. Return a Car");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("\nAvailable Cars:");
+                    for (int i = 0; i < cars.size(); i++) {
+                        Car car = cars.get(i);
+                        if (!car.isRented) {
+                            System.out.println((i + 1) + ". " + car.model + " - ₹" + car.pricePerDay + "/day");
+                        }
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("\nEnter car number to rent:");
+                    int rentChoice = sc.nextInt();
+                    if (rentChoice >= 1 && rentChoice <= cars.size()) {
+                        Car selected = cars.get(rentChoice - 1);
+                        if (!selected.isRented) {
+                            selected.isRented = true;
+                            System.out.println("You have rented " + selected.model + ". Enjoy your ride!");
+                        } else {
+                            System.out.println("Sorry, this car is already rented.");
+                        }
+                    } else {
+                        System.out.println("Invalid choice!");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("\nEnter car number to return:");
+                    int returnChoice = sc.nextInt();
+                    if (returnChoice >= 1 && returnChoice <= cars.size()) {
+                        Car selected = cars.get(returnChoice - 1);
+                        if (selected.isRented) {
+                            selected.isRented = false;
+                            System.out.println("You have returned " + selected.model + ". Thank you!");
+                        } else {
+                            System.out.println("This car was not rented.");
+                        }
+                    } else {
+                        System.out.println("Invalid choice!");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Exiting Car Rental System...");
+                    break;
+
+                default:
+                    System.out.println("Invalid input! Try again.");
+            }
+        } while (choice != 4);
+
+        sc.close();
+    }
+}
